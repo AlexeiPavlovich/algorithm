749ba39a85cbc4ce0ab0795a3262f0dac069025e
Alexei Pavlovich <Alexei.Pavlovich@Amdocs.com>
2022-12-16 15:21:27 +0200

    delete node tree

--- src/test/java/com/example/TreeTest.java
+++ src/test/java/com/example/TreeTest.java
@@ -4,25 +4,62 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+
 import org.junit.Test;
+
+import junit.framework.Assert;
 
 public class TreeTest {
 
 	@Test
 	public void TestBinarySearchTree() {
 		
-		System.out.println(Math.log(18));
+		//System.out.println(Math.log(18));
 		int[] num = { 20, 35, -15, 7, 55, 1, -22 };
+	
+
+		
 		List<Integer> list = Arrays.stream(num).boxed().collect(Collectors.toList());
 		Tree tree = new Tree();
 		list.forEach(tree::add);
 		tree.print();
 		System.out.println(tree.isBST());
+		for (int i = 0; i < num.length; i++) {
+			Assert.assertTrue(tree.contains(num[i]));
+		}
+		
+		System.out.println(tree.getMin());
+		
+		System.out.println(tree.getMax());
+		
+		
+		System.out.println("delete 15");
+		
+		tree.delete(-15);
+		
+		tree.print();
+		
+		
+		System.out.println("delete 35");
+		
+		tree.delete(35);
+		
+		tree.print();
+		
+		
+		System.out.println("delete -22");
+		
+		tree.delete(-22);
+		
+		tree.print();
+		
 		/*
 		 * for (int i = 0; i < num.length; i++) { int found = tree.search(num[i]);
 		 * Assert.assertEquals(num[i], found); } System.out.println(tree.getSorted());
 		 */
 	}
+	
+          
 
 	public static class Node {
 
@@ -66,95 +103,131 @@ public class TreeTest {
 	}
 
 	private static class Tree {
+	
 		private Node root;
 
+		public Integer getMin() {
+			return getMinRec(root);
+		}
+		
+		public Integer getMax() {
+			return getMaxRec(root);
+		}
+		
+		
+		
+		public boolean contains(Integer data) {
+			return searchRec(root, data) != null;
+		}
+		
+		
+		public void delete(Integer data) {
+			root = deleteRec(root, data);
+		}
+
+		public void add(Integer data) {
+			root = insertRec(root, data);
+		}
+		
 		public void print() {
 			printRec(root);
 		}
 		
-		//public searchRec(Integer item, Node currentNode)
-		
-		private Node searchRec(Integer item, Node currentNode) {
-			if (currentNode == null) {
-				return null;
-			} else if (currentNode.data.equals(item)) {
-				return currentNode;
-			} else if (item.compareTo(currentNode.data) < 0) {
-				return searchRec(item, currentNode.getLeft());
-			} else {
-				return searchRec(item, currentNode.getRight());
-			}
+		public boolean isBST(){
+			return isBSTRec(root);
 		}
-
-		private Integer getMin(Node node) {
+		
+		
+		private Node deleteRec(Node currNode, Integer data) {
+			if(currNode==null) {
+				return null;
+			}
+			if(data.compareTo(currNode.getData()) > 0) {
+				currNode.setRight(deleteRec(currNode.getRight(), data));
+			}
+			else if(data.compareTo(currNode.getData()) < 0) {
+				currNode.setLeft(deleteRec(currNode.getLeft(), data));
+			}
+			else {
+				//found
+				if(currNode.getLeft() == null ) {
+					return currNode.getRight();
+				}
+				else if(currNode.getRight() == null ) {
+					return currNode.getLeft();
+				}
+				
+				currNode.setData(getMinRec(currNode.getRight()));
+				currNode.setRight(deleteRec(currNode.getRight(), currNode.getData()));
+			}
+			return currNode;
+		}
+		
+		
+		private Node searchRec(Node node, Integer data) {
+			if (node == null) {
+				return null;
+			}
+			int cmp = data.compareTo(node.getData());
+			if (cmp == 0) {
+				return node;
+			} else if (cmp > 0) {
+				return searchRec(node.getRight(), data);
+			}
+			return searchRec(node.getLeft(), data);
+		}
+		
+		
+		
+		public boolean isBSTRec(Node node){
+			if(node==null) {
+				return true;
+			}
+			if(node.getLeft()!=null && getMaxRec(node.getLeft()).compareTo(node.getData()) > 0 ) {
+				return false;
+			}
+			if(node.getRight()!=null && getMinRec(node.getRight()).compareTo(node.getData()) < 0 ) {
+				return false;
+			}
+			return isBSTRec(node.getLeft()) && isBSTRec(node.getRight());
+		}
+		
+		
+		private Integer getMinRec(Node node) {
 			if (node == null) {
 				return Integer.MAX_VALUE;
 			}
-			return Math.min(node.getData(), Math.min(getMin(node.getLeft()), (getMin(node.getRight()))));
+			return Math.min(node.getData(), Math.min(getMinRec(node.getLeft()), getMinRec(node.getRight())));
 		}
-
-		private Integer getMax(Node node) {
+		
+		private Integer getMaxRec(Node node) {
 			if (node == null) {
 				return Integer.MIN_VALUE;
 			}
-			return Math.max(node.getData(), Math.max(getMax(node.getLeft()), (getMax(node.getRight()))));
+			return Math.max(node.getData(), Math.max(getMaxRec(node.getLeft()), getMaxRec(node.getRight())));
 		}
-
-		private int isBST() {
-			return isBSTRec(root);
-		}
-
-		private int isBSTRec(Node node) {
-			if (node == null) {
-				return 1;
-			}
-
-			/* false if the max of the left is > than us */
-			if (node.getLeft() != null && getMax(node.getLeft()).compareTo(node.getData()) > 0) {
-				return 0;
-			}
-
-			/* false if the min of the right is <= than us */
-			if (node.getRight() != null && getMin(node.getRight()).compareTo(node.getData()) < 0) {
-				return 0;
-			}
-
-			/*
-			 * false if, recursively, the left or right is not a BST
-			 */
-			if (isBSTRec(node.getLeft()) != 1 || isBSTRec(node.getRight()) != 1) {
-				return 0;
-			}
-
-			/* passing all that, it's a BST */
-			return 1;
-		}
-
+		
+	
 		private void printRec(Node node) {
-			if (node == null) {
+			if(node==null) {
 				return;
 			}
 			printRec(node.getLeft());
 			System.out.println(node.getData());
 			printRec(node.getRight());
+			
 		}
 
-		public void add(Integer data) {
-			root = addRec(root, data);
-		}
-
-		private Node addRec(Node node, Integer data) {
+		private Node insertRec(Node node, Integer data) {
 			if (node == null) {
-				return new Node(data);
-			}
-			if (node.getData().compareTo(data) < 0) {
-				node.setLeft(addRec(node.getLeft(), data));
-			} else {
-				node.setRight(addRec(node.getRight(), data));
+				node = new Node(data);
+			} else if (data.compareTo(node.getData()) < 0) {
+				node.setLeft(insertRec(node.getLeft(), data));
+			} else{
+				node.setRight(insertRec(node.getRight(), data));
 			}
 			return node;
 		}
-
 	}
 
 }
